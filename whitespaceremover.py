f195dfe2250fe320a6407e571b722835ad991734
JonathanPhillips <jon7035@gmail.com>
2022-04-20 15:33:51 -0400

    Add files via upload
    initial commit

---
+++ whitespaceremover.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('C:/example.csv') # read csv to dataframe
+
+def whitespace_remover(dataframe):
+     for i in dataframe.columns:
+            if dataframe[i].dtype == 'object':
+                    dataframe[i] = dataframe[i].map(str.strip)
+            else:
+                    pass
+
+whitespace_remover(df)
+print(df)  # print dataframe to console
+df.to_csv('c:example_clean.csv', index=False) # write the cleaned dataframe back to csv
